fca0527f25dd2d483d17919b3014eb2726d26853
James Tranovich <jtranovich@gmail.com>
2014-07-03 18:04:59 -0700

    Added OUTPUTNAME for weapon proficiencies.

--- src/Lst/WeaponProf.hs
+++ src/Lst/WeaponProf.hs
@@ -11,6 +11,7 @@ import Common
 data WeaponProficency = Name T.Text
                       | WeaponType [T.Text]
                       | WeaponHands Int
+                      | OutputName T.Text
                       | ProductIdentity Bool
                       | Restricted Restriction
                         deriving Show
@@ -20,7 +21,7 @@ parseProductIdentity = ProductIdentity <$> (tag "NAMEISPI" >> yesOrNo)
 
 -- comma only shows up in one file (apg_profs_weapon.lst). ugh.
 parseWordAndComma :: Parser T.Text
-parseWordAndComma = takeWhile1 $ inClass "-A-Za-z,"
+parseWordAndComma = takeWhile1 $ inClass "-A-Za-z, "
 
 parseWeaponType :: Parser WeaponProficency
 parseWeaponType = WeaponType <$> (tag "TYPE" >> parseWordAndComma `sepBy` char '.')
@@ -28,10 +29,14 @@ parseWeaponType = WeaponType <$> (tag "TYPE" >> parseWordAndComma `sepBy` char '
 parseWeaponHands :: Parser WeaponProficency
 parseWeaponHands = WeaponHands <$> (tag "HANDS" >> liftM textToInt manyNumbers)
 
+parseOutputName :: Parser WeaponProficency
+parseOutputName = OutputName <$> (tag "OUTPUTNAME" >> parseWordAndComma)
+
 parseWeaponProficencyTag :: Parser WeaponProficency
 parseWeaponProficencyTag = parseProductIdentity
                        <|> parseWeaponType
                        <|> parseWeaponHands
+                       <|> parseOutputName
                        <|> Restricted <$> parseRestriction
 
 parseWeaponProficency :: T.Text -> Parser [WeaponProficency]
